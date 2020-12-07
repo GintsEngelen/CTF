3fe764efa66b5f040c6398dcdb54dec2b4e839a6
GintsEngelen <GintsEngelen@gmail.com>
2020-12-07 22:12:11 +0100

    solved Follow The Sequence

--- 247CTF/Networking/Follow The Sequence/UncodeJSON.py
+++ 247CTF/Networking/Follow The Sequence/UncodeJSON.py
@@ -1,6 +1,8 @@
 import json
 
 
+
+
 with open("CombinedFilter", "r") as pcap:
     JSONdata = pcap.read()
 data = json.loads(JSONdata)
@@ -9,11 +11,13 @@ print(type(data))
 
 payloadList = []
 
+seqNoSet = set()
 for element in data:
     seqNo = int(element['_source']['layers']['tcp']['tcp.options_tree']['mptcp']['tcp.options.mptcp.rawdataseqno'])
-    if 'tcp.payload' in element['_source']['layers']['tcp']:
+    if 'tcp.payload' in element['_source']['layers']['tcp'] and seqNo not in seqNoSet:
         payLoad = bytes.fromhex(element['_source']['layers']['tcp']['tcp.payload'].replace(':', ''))
         payloadList.append((seqNo, payLoad))
+        seqNoSet.add(seqNo)
 
 payloadList.sort()
 
